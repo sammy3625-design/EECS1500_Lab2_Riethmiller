fd76930d4c53efa3acbcae427d4ca25e3cdb076a
sammy3625-design <sammy3625@icloud.com>
2026-01-30 23:22:54 -0500

    added awesome

---
+++ Java project/Java version.cpp
@@ -0,0 +1,11 @@
+// EECS1500_Lab2_Riethmiller
+// 1-30-26
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void) {
+
+    cout << "Hello World! This is Riethmiller .\n" << endl;
+    return 0;
+}
